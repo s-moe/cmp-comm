c70baf52d5226fe0873339e34d20de8d98cdeba0
Sarah Moe <sarahannmoe@gmail.com>
2021-11-06 12:34:59 -0700

    created custom event via emit to submit the users data

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <form>
+    <form @submit.prevent="submitData">
       <input type="text" placeholder="Your name" v-model="enteredName" />
       <input type="text" placeholder="Your age" v-model="enteredAge" />
       <button>Set User Data</button>
@@ -10,11 +10,17 @@
 
 <script>
 export default {
+  emits: ["set-data"],
   data() {
     return {
       enteredName: "",
       enteredAge: "",
     };
+  },
+  methods: {
+    submitData() {
+      this.$emit("set-data", this.enteredName, this.enteredAge);
+    },
   },
 };
 </script>
