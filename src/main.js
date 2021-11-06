18817d12500ab56301d45cdd7f2b5fbe7fa9a2e8
Sarah Moe <sarahannmoe@gmail.com>
2021-11-06 12:09:13 -0700

    imported ActiveUser and UserData into main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
+import ActiveUser from "./components/ActiveUser.vue";
+import UserData from "./components/UserData.vue";
+createApp({}).mount("#app");
 
-createApp({}).mount('#app')
-
-// Task 1: 
-// Add two components to the app: 
+// Task 1:
+// Add two components to the app:
 // An ActiveUser component and an UserData component
 // ActiveUser should output a username (h2) and age (h3)
 // UserData should output two input fields => for name and age
