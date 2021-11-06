be494d1f8b036820b8fee5c5d48de90eec2a9dcf
Sarah Moe <sarahannmoe@gmail.com>
2021-11-06 12:31:54 -0700

    bound data to input fields

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -1,10 +1,20 @@
 <template>
   <section>
-    <input type="text" placeholder="Your name" />
-    <input type="text" placeholder="Your age" />
+    <form>
+      <input type="text" placeholder="Your name" v-model="enteredName" />
+      <input type="text" placeholder="Your age" v-model="enteredAge" />
+      <button>Set User Data</button>
+    </form>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: "",
+    };
+  },
+};
 </script>
